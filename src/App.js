b0312e60c12e2ad5b3bbfb3abd3fcffc97e3f67e
Timothy Seah <timothyseah.t.e@gmail.com>
2019-03-30 17:33:37 +0800

    Display gang member data on cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React, { Component } from "react";
-import unsplash from "./service/unsplash"
+import unsplash from "./service/unsplash";
 import Card from "./components/Card/Card";
 
 class App extends Component {
   state = {
     resultList: []
   };
+  handleClick = () => {};
 
   async componentDidMount() {
-    const searchTerm = "trees"
-    const response= await unsplash.get("/search/photos", {
-      params: {query: searchTerm}
+    const fs = require("fs");
+
+    const searchTerm = "";
+    const response = await unsplash.get("/api/v1/gangs/Ku Klutz Katz/", {
+      params: { query: searchTerm }
     });
-    this.setState({resultList: response.data.results})
+    const description = response.data.description;
+    const logo = response.data.logo;
+    console.log(response);
+
+    const mugshots = response.data.members[0].mugshot.data.data;
+    console.log(response.data.members[0].mugshot);
+
+    var blobby = new Blob([new Uint8Array(mugshots)]);
+    // var blobToFile = (theBlob, fileName) => {
+    //   //A Blob() is almost a File() - it's just missing the two properties below which we will add
+    //   theBlob.lastModifiedDate = new Date();
+    //   theBlob.name = fileName;
+    //   return theBlob;
+    // };
+
+    var objectURL = URL.createObjectURL(blobby);
+
+    console.log(objectURL);
+    var image = new Image();
+    image.src = objectURL;
+
+    // tryagain = URL.createObjectURL(imageSource);
+    const memberNames = [];
+    const mugshotsArray = [];
+    response.data.members.forEach(member => {
+      memberNames.push(member.name);
+      mugshotsArray.push(member.mugshot);
+    });
+
+    console.log(mugshotsArray);
+    this.setState({
+      resultList: memberNames,
+      imageSource: objectURL
+    });
   }
 
   render() {
-    const { resultList } = this.state;
+    const { resultList, imageSource } = this.state;
     return (
       <div className="container">
         {resultList.map(result => (
-          <Card imageUrl={result.urls.small} title={result.description} />
+          <Card
+            imageUrl={imageSource}
+            title={result}
+            buttonPress={this.handleClick}
+          />
+        ))}
+        {resultList.map(result => (
+          <Card
+            imageUrl={imageSource}
+            title={result}
+            buttonPress={this.handleClick}
+          />
         ))}
       </div>
     );
--- src/service/unsplash.js
+++ src/service/unsplash.js
@@ -1,8 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.unsplash.com",
+  baseURL: "http://canine-criminologist.herokuapp.com",
   headers: {
     Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`
   }
-})
+});
